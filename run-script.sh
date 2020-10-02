f1de6f84de686e7a69d8984a79be8d1a2476d21f
Vinamra Jain <vinjain@redhat.com>
2020-10-02 11:12:08 +0530

    Add script to install tasks for pipeline

---
+++ run-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# for blue-green-deployment pipeline
+
+# creating pvc, secrets, configmaps and serviceaccount
+kubectl create -f creds/quay-secret.yaml
+kubectl create -f creds/serviceaccount.yaml
+kubectl create cm tokens-configmap --from-file=misc/tokens.json
+kubectl create -f pvcs/shared-workspaces.yaml
+
+# installing tasks from upstream catalog
+kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-clone/0.2/git-clone.yaml
+kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/replace-tokens/0.1/replace-tokens.yaml
+kubectl create -f https://raw.githubusercontent.com/vinamra28/catalog/vinamra28/sonar-fix/sonarqube/sonarqube.yaml
+kubectl create -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/buildah/0.1/buildah.yaml
+# installing the pipeline
+kubectl create -f pipeline.yaml
